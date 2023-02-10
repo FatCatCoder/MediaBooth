64973957949c99c32b271497e1d852a18111b41d
Christian Claudeaux <christian75armand@gmail.com>
2023-02-10 00:21:28 -0500

    Initial Commit

---
+++ MediaBooth/MediaBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaBoothApp.swift
+//  MediaBooth
+//
+//  Created by christian claudeauex on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MediaBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
